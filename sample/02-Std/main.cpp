9db70de91eee0be747bf5fbcdf72ee771a200c78
tacigar <ig.lhound@gmail.com>
2017-04-29 00:56:14 +0900

    Add a sample '02-Std'.

---
+++ sample/02-Std/main.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <luatic/namespace.hpp>
+#include <luatic/global.hpp>
+
+auto main() -> int {
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+    if (luaL_loadfile(L, "main.lua")) {
+        std::cerr << "Cannot open main.lua." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    luatic::Global(L)
+        .begin()
+            .defNamespace("std")
+            .begin()
+                .defNamespace("math")
+                .begin()
+                    .defFunction("sin", [](double x)->double { return std::sin(x); })
+                    .defFunction("cos", [](double x)->double { return std::cos(x); })
+                    .defFunction("tan", [](double x)->double { return std::tan(x); })
+                .end()
+                .defNamespace("io")
+                .begin()
+                    .defFunction("print_number", [](double x)->void {
+                            std::cout << x << std::endl;
+                        })
+                    .defFunction("print_string", [](const std::string& s)->void {
+                            std::cout << s << std::endl;
+                        })
+                .end()
+            .end()
+        .end();
+    
+    if (lua_pcall(L, 0, 0, 0)) {
+        std::cerr << "Cannot exec main.lua:" << lua_tostring(L, -1) << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
---
+++ sample/02-Std/main.lua
@@ -0,0 +1,2 @@
+std.io.print_number(std.math.sin(1))
+std.io.print_number(std.math.cos(1))
